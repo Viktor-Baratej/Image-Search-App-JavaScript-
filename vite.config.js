b6a073af3752d1c93da0007753653ec162ca872c
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2025-05-13 14:37:40 +0300

    add fix base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+      base: '/Image-Search-App-JavaScript-/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
